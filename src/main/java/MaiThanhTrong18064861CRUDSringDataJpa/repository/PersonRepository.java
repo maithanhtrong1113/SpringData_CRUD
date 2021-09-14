d4cd7288a9532aca67c50880856f6d0220c4e3e0
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2021-09-14 23:00:01 +0700

    native

--- src/main/java/MaiThanhTrong18064861CRUDSringDataJpa/repository/PersonRepository.java
+++ src/main/java/MaiThanhTrong18064861CRUDSringDataJpa/repository/PersonRepository.java
@@ -1,11 +1,35 @@
 package MaiThanhTrong18064861CRUDSringDataJpa.repository;
 
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import MaiThanhTrong18064861CRUDSringDataJpa.entity.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
+	@Query(value = "select * from person", nativeQuery = true)
+	List<Person> getAll();
 	
+	@Query(value = "select * from person where name = ?1", nativeQuery = true)
+	List<Person> getListByName(String name);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "insert into person(name) values(?1)", nativeQuery = true)
+	void addPerson(String name);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "update person set name = ?2 where id = ?1", nativeQuery = true)
+	void updatePerson(Integer id, String name);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "delete from person where id = ?1", nativeQuery = true)
+	void deletePersonById(Integer id);
 }
--- src/test/java/MaiThanhTrong18064861CRUDSringDataJpa/PersonRepositoryTest.java
+++ src/test/java/MaiThanhTrong18064861CRUDSringDataJpa/PersonRepositoryTest.java
@@ -1,9 +1,39 @@
 package MaiThanhTrong18064861CRUDSringDataJpa;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import MaiThanhTrong18064861CRUDSringDataJpa.repository.PersonRepository;
 
 @SpringBootTest
 public class PersonRepositoryTest {
 
+	@Autowired
+	private PersonRepository personRepository;
 	
+	@Test
+	public void getAll() {
+
+		System.out.println("getAll: " + personRepository.getAll());
+		System.out.println("getListByName: " + personRepository.getListByName("Trong"));
+	}
+
+	@Test
+	public void add() {
+
+		personRepository.addPerson("Trong123");
+	}
+
+	@Test
+	public void update() {
+
+		personRepository.updatePerson(1, "Trongasd");
+	}
+
+	@Test
+	public void delete() {
+
+		personRepository.deletePersonById(1);
+	}
 }
