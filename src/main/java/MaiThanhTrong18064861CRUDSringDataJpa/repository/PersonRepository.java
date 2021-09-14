25bfc20bf020272ab5dac9e16f2333d67f45f777
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2021-09-14 23:39:37 +0700

    repo

--- src/main/java/MaiThanhTrong18064861CRUDSringDataJpa/repository/PersonRepository.java
+++ src/main/java/MaiThanhTrong18064861CRUDSringDataJpa/repository/PersonRepository.java
@@ -1,11 +1,13 @@
 package MaiThanhTrong18064861CRUDSringDataJpa.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import MaiThanhTrong18064861CRUDSringDataJpa.entity.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
-	
+	List<Person> findByName(String name);
 }
--- src/test/java/MaiThanhTrong18064861CRUDSringDataJpa/PersonRepositoryTest.java
+++ src/test/java/MaiThanhTrong18064861CRUDSringDataJpa/PersonRepositoryTest.java
@@ -1,9 +1,44 @@
 package MaiThanhTrong18064861CRUDSringDataJpa;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import MaiThanhTrong18064861CRUDSringDataJpa.entity.Person;
+import MaiThanhTrong18064861CRUDSringDataJpa.repository.PersonRepository;
 
 @SpringBootTest
 public class PersonRepositoryTest {
+
+	@Autowired
+	private PersonRepository personRepository;
+
+	@Test
+	public void getAll() {
+
+		System.out.println("getAll: " + personRepository.findAll());
+		System.out.println("getListByName: " + personRepository.findByName("Tien"));
+	}
+
+	@Test
+	public void add() {
+
+		Person person = Person.builder().name("Tien Huynh 123").build();
+		System.out.println("Save Person: " + personRepository.save(person));
+	}
+
+	@Test
+	public void update() {
+
+		Person person = Person.builder().id(1).name("Tien Doi Ten").build();
+		System.out.println("Update Person: " + personRepository.save(person));
+	}
+
+	@Test
+	public void delete() {
+
+		personRepository.deleteById(1);
+	}
 
 	
 }
